9995e6c9b5c0e6f00b948fd0727552ec34895aba
AlmazN <almazn2015@gmail.com>
2017-01-27 11:54:11 +0300

    issue in ShopingCartComp fixed, now productsInCart has correct type CartProduct[]

--- src/app/shoping-cart/shoping-cart.component.ts
+++ src/app/shoping-cart/shoping-cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ShopingCartService } from '../shoping-cart.service';
 import { Product } from '../product/product.model';
+import { CartProduct } from '../product/cart-product.model';
 
 @Component({
   selector: 'app-shoping-cart',
@@ -8,7 +9,7 @@ import { Product } from '../product/product.model';
   styleUrls: ['./shoping-cart.component.css']
 })
 export class ShopingCartComponent implements OnInit {
-  productsInCart: Product[];
+  productsInCart: CartProduct[];
 
   constructor(private cartService: ShopingCartService) { }
 
