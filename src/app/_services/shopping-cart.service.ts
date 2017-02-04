667d130ce03f74e1d879acf673e1f8011592ec4d
AlmazN <almazn2015@gmail.com>
2017-02-04 15:26:01 +0300

    shoping-cart service code optomization

--- src/app/_services/shopping-cart.service.ts
+++ src/app/_services/shopping-cart.service.ts
@@ -6,38 +6,24 @@ import { Subject } from 'rxjs/Subject';
 @Injectable()
 export class ShopingCartService {
   private cartProducts: CartProduct[] = [];
-  productsQuantity: number;
+  productsQuantity: number = 0;
   productQuantitySource = new Subject<number>();
   changeProductQuantity$ = this.productQuantitySource.asObservable();
 
   addProduct(product: Product): void {
-    let i = this.arrayObjectIndexOf(this.cartProducts, product.name, 'product', 'name');
-    if (i > -1) {
-      this.cartProducts[i]['quantity']++;
+    let found = this.cartProducts.find(p => p.product.id === product.id);
+    if (found) {
+      found.quantity++;
     } else {
       this.cartProducts.push(new CartProduct(product));
     }
-    this.productQuantitySource.next(this.getProductsQuantity());
+    this.productsQuantity++;
+    this.productQuantitySource.next(this.productsQuantity);
   }
 
   getCartProducts(): CartProduct[] {
     return this.cartProducts;
   }
-
-  getProductsQuantity(): number {
-    this.productsQuantity = 0;
-    for (let product of this.cartProducts) {
-      this.productsQuantity += product.quantity;
-    }
-    return this.productsQuantity;
-  }
-
-  arrayObjectIndexOf(myArray, searchTerm, property1, property2) {
-    for(let i = 0, len = myArray.length; i < len; i++) {
-        if (myArray[i][property1][property2] === searchTerm) return i;
-    }
-    return -1;
-}
 
   constructor() { }
 
