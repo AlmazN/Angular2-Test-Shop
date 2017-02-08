a4c077060bed125d40953c17a23aebb7b29e81c2
AlmazN <almazn2015@gmail.com>
2017-02-08 13:18:43 +0300

    just some fix

--- src/app/shopping-cart/shopping-cart.component.spec.ts
+++ src/app/shopping-cart/shopping-cart.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { ShopingCartComponent } from './shoping-cart.component';
+import { ShopingCartComponent } from './shopping-cart.component';
 
 describe('ShopingCartComponent', () => {
   let component: ShopingCartComponent;
