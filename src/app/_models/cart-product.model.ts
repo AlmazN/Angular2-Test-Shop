323eec0e6259a2aeb6e9c6c24812a98bad8ea9e3
AlmazN <almazn2015@gmail.com>
2017-02-10 11:25:29 +0300

    cart-product model refactor

--- src/app/_models/cart-product.model.ts
+++ src/app/_models/cart-product.model.ts
@@ -1,11 +1,20 @@
 import { Product } from './product.model';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 export class CartProduct {
     product: Product;
-    quantity?: number;
+    private _quantitySource? = new BehaviorSubject<number>(0);
+    quantity$ = this._quantitySource.asObservable();
+
+    get quantity(): number {
+        return this._quantitySource.getValue();
+    }
+
+    set quantity(newQuantity: number) {
+        this._quantitySource.next(newQuantity);
+    }
 
     constructor(product: Product) {
         this.product = product;
-        this.quantity = 1;
     }
 }
