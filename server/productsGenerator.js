d50798a70bd21abf7ef6498c3ab4c666f2ee767b
AlmazN <almazn2015@gmail.com>
2017-02-26 16:57:09 +0300

    productsGenerator refactor

--- server/productsGenerator.js
+++ server/productsGenerator.js
@@ -8,6 +8,25 @@ objEn = {
   products: []
 };
 
+function productName(wordsNumber) {
+  switch(wordsNumber) {
+    case 1:
+      return faker.commerce.product();
+      break;
+    case 2:
+      return `${faker.commerce.productAdjective()} ${faker.commerce.product()}`;
+      break;
+    case 3:
+      return `${faker.commerce.productMaterial()} ${faker.commerce.productAdjective()} ${faker.commerce.product()}`;
+      break;
+    case 4:
+      return `${faker.commerce.productMaterial()} ${faker.commerce.productAdjective()} ${faker.commerce.product()} ${faker.commerce.product()}`;
+      break;
+    default:
+      return `${faker.commerce.productAdjective()} ${faker.commerce.product()}`;
+  }
+}
+
 for(i = 1; i <= 100; i++) {
   let generatedPrice = faker.random.number(5000),
       generatedQuantity = faker.random.number(5000),
@@ -16,9 +35,8 @@ for(i = 1; i <= 100; i++) {
 
   objRu.products.push({
     id: i,
-    name: random > 5 ? random < 8 ? faker.commerce.product() : `${faker.commerce.productAdjective()} ${faker.commerce.product()}`
-    : random > 2 ? `${faker.commerce.productMaterial()} ${faker.commerce.productAdjective()} ${faker.commerce.product()}`: 
-    `${faker.commerce.productMaterial()} ${faker.commerce.productAdjective()} ${faker.commerce.product()} ${faker.commerce.product()}`,
+    name: random > 5 ? random < 8 ? productName(1) : productName(2) :
+          random > 2 ? productName(3) : productName(4),
     description: faker.lorem.paragraph(),
     price: generatedPrice,
     quantity: random > 2 ? generatedQuantity : 0,
